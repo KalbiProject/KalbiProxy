b1058595edffae88be306496a14437d27e118b33
Aaron Parfitt <aaronparfitt123@gmail.com>
2020-10-12 19:25:19 +0100

    made it work

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -2,6 +2,7 @@ package proxy
 
 import (
 	"fmt"
+	//"strconv"
 	"github.com/KalbiProject/Kalbi"
 	"github.com/KalbiProject/Kalbi/sip/message"
 	"github.com/KalbiProject/Kalbi/sip/method"
@@ -38,16 +39,23 @@ func (p *Proxy) HandleRequest(tx transaction.Transaction) {
 			tx.Send(msg, string(tx.GetOrigin().Contact.Host), string(tx.GetOrigin().Contact.Port))
 
 		} else {
+			user := strings.Split(user, ":")
 			msg2 := message.NewRequest(method.INVITE, string(tx.GetOrigin().To.User)+"@"+string(tx.GetOrigin().To.Host), string(tx.GetOrigin().From.User)+"@"+string(tx.GetOrigin().From.Host))
 			msg2.CopyHeaders(tx.GetOrigin())
 			msg2.CopySdp(tx.GetOrigin())
+            msg2.To.Host = []byte(user[0])
+			msg2.Via[0].Host = []byte("192.168.10.122")
 			msg2.Via[0].SetBranch(transaction.GenerateBranchId())
+			msg2.Contact.Host = []byte("192.168.10.122")
 			msg2.Contact.Port = []byte("5060")
+			msg2.Via[0].Port = []byte("5060")
 			TxMng := p.stack.GetTransactionManager()
 			ctx := TxMng.NewClientTransaction(msg)
-			ctx.ServerTxID = string(tx.GetOrigin().Via[0].Branch)
-			TxMng.PutTransaction(ctx)
-			user := strings.Split(user, ":")
+			ctx.ServerTxID = string(tx.GetBranchID())
+			fmt.Println("Branch: " + string(tx.GetLastMessage().Via[0].Branch))
+			fmt.Println(msg2.Sdp.Export())
+			fmt.Printf("SDP SIZE: %d", len(msg2.Sdp.Export()))
+            //msg2.ContLen.SetValue(strconv.Itoa(msg.Sdp.Size()))
 			ctx.Send(msg2, user[0], user[1])
 
 		}
@@ -81,15 +89,34 @@ func (p *Proxy) HandleResponse(response transaction.Transaction) {
 	 
 	 if response.GetLastMessage().GetStatusCode() == 100 {
 		 return
-		 
+
+	} else if response.GetLastMessage().GetStatusCode() == 180 {
+		 return
 	
-	 } else{
-		  fmt.Println("I GET HERE ")
-		  tx := response.GetServerTransaction()
-		  if tx == nil {
-			fmt.Println("Somthing is wrong")
+	 } else {
+		  
+		  TxMng := p.stack.GetTransactionManager()
+		  tx, exists := TxMng.FindServerTransactionByID(response.GetServerTransactionID())
+		  if exists == false {
+			  fmt.Println("Server Tranaction has been terminated as 200 OK has been sent, must send statlessly ")
+			  msg := message.NewResponse(status.OK, "@", "@")
+			  msg.CopyHeaders(response.GetOrigin())
+			  msg.CopySdp(response.GetOrigin())
+			  response.Send(msg, string(response.GetLastMessage().Contact.Host), string(response.GetLastMessage().Contact.Port))
+		  }else {
+			fmt.Println("Server Tranaction Found")
+			msg := message.NewResponse(status.OK, "@", "@")
+
+			fmt.Println(string(response.GetLastMessage().Src))
+			msg.CopyHeaders(response.GetLastMessage())
+			msg.Via[0].SetBranch(tx.GetBranchID())
+			msg.CopySdp(response.GetLastMessage())
+			//msg.ContLen.SetValue(strconv.Itoa(msg.Sdp.Size()))
+			fmt.Println(string(msg.Export()))
+			response.Send(msg, string(tx.GetOrigin().Contact.Host), string(tx.GetOrigin().Contact.Port))
+
 		  }
-		  //tx.Send(response.GetLastMessage(), string(tx.GetOrigin().Contact.Host), string(tx.GetOrigin().Contact.Port) )
+	
 	 }
 
     
@@ -118,7 +145,8 @@ func (p *Proxy) ServeResponses() {
 func (p *Proxy) Start() {
 	p.RegisteredUsers = make(map[string]string)
 	p.stack = kalbi.NewSipStack("Basic")
-	p.stack.CreateListenPoint("udp", "0.0.0.0", 5060)
+	p.stack.CreateListenPoint("udp", "192.168.10.122", 5060)
+	//p.stack.CreateListenPoint("udp", "127.0.0.1", 5060)
 	p.requestschannel = p.stack.CreateRequestsChannel()
 	p.responseschannel = p.stack.CreateResponseChannel()
 	go p.stack.Start()
